de6a09f90da98f1d847bce1dc63c08cc9de03178
sigmotoa <si.galvismotoa@gmail.com>
2024-04-10 13:00:29 -0500

    ciclo for

--- Basica_12mod/Program.cs
+++ Basica_12mod/Program.cs
@@ -18,13 +18,18 @@ int fin = Convert.ToInt32(Console.ReadLine());
 int i = 0;
 int[] sumar = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int[] producto = new[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-while (i < fin)
-{
-    sumar[i] = producto[i] * 3;
-    Console.WriteLine($"En la posición: {i}\tsumar:{sumar[i]} \tproducto: {producto[i]}");
-    i++;
+//while (i < fin)
+//{
+  //  sumar[i] = producto[i] * 3;
+    //Console.WriteLine($"En la posición: {i}\tsumar:{sumar[i]} \tproducto: {producto[i]}");
+    //i++;
     //Console.WriteLine(sumar[i]);
     //i = i+1;
-}
-Console.WriteLine(sumar);
+//}
+//Console.WriteLine(sumar);
+
+    for (int j = 1; j < fin; j=j+2)
+    {
+        Console.WriteLine(j);
+    }
 
